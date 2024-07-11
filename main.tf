59e6d11efe97bac8b914e8f2503dbc915a5906e0
EQIX-TAM-JP <snakayama@ap.equinix.com>
2024-07-11 15:14:43 +0900

    ..

--- main.tf
+++ main.tf
@@ -1,3 +1,59 @@
+data "equinix_network_device" "vd_pri" {
+  name = "vd-${var.metro_code}-${var.username}-pre"
+}
+data "equinix_network_device" "vd_sec" {
+  name = "vd-${var.sec_metro_code}-${var.username}-sec"
+}
+
+# IOS-XE configuration
+provider "iosxe" {
+  alias    = "vd_pri"
+  username = "${var.username}"
+  password = "${data.equinix_network_device.vd_pri.vendor_configuration.adminPassword}"
+  url      = "https://${data.equinix_network_device.vd_pri.ssh_ip_address}"
+}
+provider "iosxe" {
+  alias    = "vd_sec"
+  username = "${var.username}"
+  password = "${data.equinix_network_device.vd_sec.vendor_configuration.adminPassword}"
+  url      = "https://${data.equinix_network_device.vd_sec.ssh_ip_address}"
+}
+
+resource "iosxe_interface_ethernet" "interface" {
+  type                           = "GigabitEthernet"
+  name                           = var.int
+  bandwidth                      = var.bw
+  description                    = var.int_desc
+  shutdown                       = false
+  ip_proxy_arp                   = false
+  ip_redirects                   = false
+  ip_unreachables                = false
+  ipv4_address                   = cidrhost("${var.network_range}/${var.cidr}", 3)
+  ipv4_address_mask              = var.cidr
+  snmp_trap_link_status            = true
+  logging_event_link_status_enable = true
+}
+
+resource "iosxe_bgp" "bgp" {
+  asn                  = var.vnf_asn
+  log_neighbor_changes = true
+}
+
+resource "iosxe_bgp_address_family_ipv4" "ipv4" {
+  asn                   = var.vnf_asn
+  af_name               = "unicast"
+  ipv4_unicast_redistribute_connected = true
+  ipv4_unicast_redistribute_static = true  
+}
+
+resource "iosxe_bgp_ipv4_unicast_neighbor" "neighbor" {
+  asn                   = var.vrf_asn
+  ip                    = cidrhost("${var.network_range}/${var.cidr}", 2)
+  activate                = true
+  soft_reconfiguration = "inbound"
+}
+
+# setup for metal environment
 resource "equinix_metal_vrf" "myvrf" {
   description = var.vrf_desc
   name        = var.vrf_name
@@ -6,8 +62,6 @@ resource "equinix_metal_vrf" "myvrf" {
   ip_ranges   = var.vrf_ranges
   project_id  = var.project_id
 }
-
-# Create Metal Gateway for a VLAN with a private IPv4 block with 8 IP addresses
 
 resource "equinix_metal_vlan" "myvlan" {
   description = var.vlan_desc
@@ -31,17 +85,6 @@ resource "equinix_metal_gateway" "mygateway" {
   ip_reservation_id = equinix_metal_reserved_ip_block.myrange.id
 }
 
-# provider "iosxe" {
-#   username = var.username
-#   password = module.ne.pass
-#   url      = "https://${module.ne.ssh_ip_vd}"
-# }
 
-# resource "iosxe_cli" "example" {
-#   cli = <<-EOT
-#   interface Lo0
-#   desc testing
-#   EOT
-# }
 
 
