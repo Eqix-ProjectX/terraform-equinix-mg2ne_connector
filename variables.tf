ab8488a1d1c75f3e0ad0ce498f2ae94da5d86707
EQIX-TAM-JP <regulate0109@gmail.com>
2024-08-03 13:08:14 +0900

    remove ne

--- variables.tf
+++ variables.tf
@@ -8,10 +8,10 @@ variable "vlan_desc" {}
 variable "range_desc" {}
 variable "cidr" {}
 variable "network_range" {}
-variable "metro_code" {}
-variable "sec_metro_code" {}
-variable "username" {}
-variable "int" {}
-variable "int_desc" {}
-variable "bw" {}
-variable "vnf_asn" {}
+# variable "metro_code" {}
+# variable "sec_metro_code" {}
+# variable "username" {}
+# variable "int" {}
+# variable "int_desc" {}
+# variable "bw" {}
+# variable "vnf_asn" {}
